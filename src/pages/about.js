e289df532ae973769ffc9c99870e67647c2026f1
abhay-25 <129974329+abhay-25@users.noreply.github.com>
2023-10-06 21:10:11 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -56,11 +56,11 @@ const about = () => {
                 Biography
               </h2>
               <p className="font-medium">
-                Hi, I am Abhay, a web developer and UI/UX designer with a
+                Hi, I am Abhay, a full stack web developer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. I am just a beginner in the field. I
                 am always looking for new and innovative ways to bring my
-                clients visions to life.
+                my visions to life.
               </p>
               <p className="my-4 font-medium">
                 I believe that design is about more than just making things look
@@ -68,8 +68,7 @@ const about = () => {
                 enjoyable experiences for users.
               </p>
               <p className="font-medium">
-                Whether I am working on a website, mobile app, or other digital
-                product, I bring my commitment to design excellence and
+                Whenever, I am working on a website, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
                 your next project.
